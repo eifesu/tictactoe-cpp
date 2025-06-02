444e8d8d363a11883a80edc49b5779f6037a164f
Chris YESSO <yessochrisa@gmail.com>
2025-06-02 10:27:10 +0100

    Working product

---
+++ README.md
@@ -0,0 +1,32 @@
+# Tic Tac Toe in C++
+
+## Overview
+
+Welcome to the Tic Tac Toe project! This project is designed to help you get accustomed to programming in C++. It serves as a great introduction to basic C++ concepts such as classes, functions, control structures, and standard input/output operations.
+
+In this game, two players take turns marking a 3x3 grid with their respective symbols (usually 'X' and 'O'). The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.
+
+## Features
+
+-   Two-player mode
+-   Simple text-based UI
+-   Clear and reset the game board
+-   Display the current game state (e.g., which player's turn, game result)
+
+## Screenshot
+
+![Tic Tac Toe UI](assets/screenshot.png)
+
+## Getting Started
+
+### Prerequisites
+
+Make sure you have a C++ compiler installed on your system. You can use GCC, Clang, or any other C++ compiler of your choice.
+
+### Compiling the Project
+
+To compile the project, navigate to the project directory and run the following command:
+
+```sh
+g++ -o tic_tac_toe main.cpp -std=c++11
+```
--- game.cpp
+++ game.cpp
@@ -2,12 +2,13 @@
 #include <iostream>
 #include <cstdlib>
 #include <unistd.h>
+#include <cmath>
 
 enum Piece
 {
-    EMPTY,
-    P1,
-    P2
+    EMPTY = 1,
+    P1 = 2,
+    P2 = 3
 };
 
 enum State
@@ -28,7 +29,7 @@ public:
         {
             for (int j = 0; j < 3; j++)
             {
-                this->board[i][j] = EMPTY;
+                this->board[i][j] = Piece::EMPTY;
             }
         }
     }
@@ -39,6 +40,7 @@ class Game
 private:
     Board b = Board{};
     bool isPlayerOneTurn = true;
+    State s = State::IDLE;
 
 public:
     Game()
@@ -57,6 +59,7 @@ public:
     void update()
     {
         clean();
+        updateGameState();
         render();
     };
     void clean()
@@ -64,6 +67,23 @@ public:
         system("clear");
     }
     void render()
+    {
+        renderHeader();
+        renderBoard();
+        renderFooter();
+    };
+
+    void renderFooter()
+    {
+        std::cout << renderGameState() << std::endl;
+    }
+
+    void renderHeader()
+    {
+        std::cout << "Player " << (this->isPlayerOneTurn ? "1" : "2") << "'s turn" << std::endl;
+    }
+
+    void renderBoard()
     {
         for (int i = 0; i < 3; i++)
         {
@@ -73,22 +93,91 @@ public:
             }
             std::cout << std::endl;
         }
-    };
+    }
     void renderPiece(Piece p)
     {
         switch (p)
         {
-        case EMPTY:
+        case Piece::EMPTY:
             std::cout << "#";
             break;
-        case P1:
+        case Piece::P1:
             std::cout << "X";
             break;
-        case P2:
+        case Piece::P2:
             std::cout << "O";
             break;
         }
     };
+
+    void updateGameState()
+    {
+        int col[3] = {1, 1, 1};
+        bool draw = true;
+
+        for (int i = 0; i < 3; i++)
+        {
+            int row = 1;
+            for (int j = 0; j < 3; j++)
+            {
+                row *= this->b.board[i][j];
+                col[j] *= this->b.board[i][j];
+                if (this->b.board[i][j] == Piece::EMPTY)
+                {
+                    draw = false;
+                }
+            }
+            if (row == pow(Piece::P1, 3))
+            {
+                this->s = State::W1;
+                return;
+            }
+            if (row == pow(Piece::P2, 3))
+            {
+                this->s = State::W2;
+                return;
+            }
+        }
+        if (draw)
+        {
+            this->s = State::DRAW;
+            return;
+        }
+
+        for (auto &&i : col)
+        {
+            if (i == pow(Piece::P1, 3))
+            {
+                this->s = State::W1;
+                return;
+            }
+            if (i == pow(Piece::P2, 3))
+            {
+                this->s = State::W2;
+                return;
+            }
+        }
+    }
+
+    std::string renderGameState()
+    {
+        switch (this->s)
+        {
+        case State::IDLE:
+            return "IDLE";
+        case State::DRAW:
+            return "DRAW";
+        case State::W1:
+            return "W1";
+        case State::W2:
+            return "W2";
+        default:
+            return "IDLE";
+        };
+
+        return "IDLE";
+    }
+
     void onCharInput(char c)
     {
         if (c >= '0' && c <= '9')
@@ -98,13 +187,13 @@ public:
             int r = (n == 1) ? 0 : (n - 1) / 3;
             int c = (n - 1) % 3;
 
-            this->b.board[r][c] = isPlayerOneTurn ? P1 : P2;
+            this->b.board[r][c] = isPlayerOneTurn ? Piece::P1 : Piece::P2;
             this->isPlayerOneTurn = !isPlayerOneTurn;
         }
     };
     void loop()
     {
-        while (true)
+        while (true && this->s == State::IDLE)
         {
             char c = getchar();
             if (c != EOF)
